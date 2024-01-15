091c2c657efa0d41ac2865b2fa6e26546e4207e6
ecubaj22 <128150148+ecubaj22@users.noreply.github.com>
2024-01-15 04:20:58 +0100

    Add files via upload
    This is a Slider Demo i made to practice my skills in java

---
+++ Main.java
@@ -0,0 +1,23 @@
+package practice;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+public class Main {
+
+	public static void main(String[] args) {
+		SliderDemo sliderDemo = new SliderDemo();
+	}
+}
